1bbf1173ce82224776829f9582b4cf7b6ecfb21e
Joshua Williams <jwilliams199055@gmail.com>
2024-01-15 22:57:27 -0500

    updated basic functionality of typing game, added notes to readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # typeracer
 Simple Typing Game 
+
+# Notes
+
+1. Setup websocket server
+2. Setup websocket service in app, to connect to the server
+3. Further elaborate on the websocket events necessary to run the game
+4. Enhance UI to trigger start game, and automatically trigger end game
+  - Add Refresh button
+  - Add start game button
+  - Make words Display easier to view
+  - Center game hud
+  - Progress Bar as a visual representation of score 
--- app/src/app/components/game-container/game-container.component.ts
+++ app/src/app/components/game-container/game-container.component.ts
@@ -5,44 +5,41 @@ import { AfterViewInit, Component, OnInit, ViewChild, ElementRef } from '@angula
   templateUrl: './game-container.component.html',
   styleUrls: ['./game-container.component.scss']
 })
-export class GameContainerComponent implements OnInit  {
+export class GameContainerComponent  {
 
   @ViewChild('gameInput') gameInput: ElementRef<HTMLInputElement>;
 
-  paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit Quidem magnam iste eos mollitia saepe minus quae recusandae culpa dolorem ratione consequatur perferendis totam omnis quibusdam beatae eum excepturi odit assumenda!';
-
+  paragraph = 'Lorem ipsum dolor sit amet consectetur';
   words: string[] = this.paragraph.split(' ');
-
   cursor = 0;
+  points = 0;
 
   get inputValue(): string {
     return this.gameInput.nativeElement.value;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
-
-
   onKeydown(event: KeyboardEvent) {
 
     if ( event.code === 'Space' ) {
       // add handling for when the cursor reaches the end of the array
-      this.verifyWord(this.inputValue)
-      this.gameInput.nativeElement.value = '';
-      return event.preventDefault();
+      if (this.cursor < this.words.length - 1 ) {
+        this.verifyWord(this.inputValue);
+        this.gameInput.nativeElement.value = '';
+      } else {
+        alert(`You scored ${this.points} / ${this.words.length}`);
+      }
 
+      return event.preventDefault();
     }
     // highlight the letters that are correct / wrong
   }
 
   verifyWord(textInput: string) {
     // check if textwInput matches words[cursor], update screen accordingly
-    console.log(
-      'this.words[this.cursor++] === textInput =',  this.words[this.cursor] === textInput
-    )
+    if (this.words[this.cursor] === textInput) {
+      this.points++;
+    }
+
     this.cursor++;
   }
 }
